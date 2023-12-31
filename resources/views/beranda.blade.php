bb1ea593515b3f40545699812755252c10722ba4
rahmaalia <rahmaaliaputri27@gmail.com>
2023-12-31 14:25:33 +0700

    beranda, kategori, jenis

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -2,3 +2,59 @@
 @extends('layouts.main')
 
 @section('container')
+<div class="container-fluid">
+    
+    <p class="fs-3 mt-4 ">Hello Mitra Kurir </p>
+
+    <div class="card mb-3" style="max-width: 500px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="https://source.unsplash.com/170x137?electronics" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body" style="background-color: #9BDCBD">
+              <h5 class="card-title">Kamu sudah membawa</h5>
+              <h2 class="card-text fw-bold">250</h2>
+              <p class="card-text"><small class="text-body-secondary">Sampah elektronik</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0 mt-5">
+        <div class="col-sm-6 col-md-8">
+            <h5 class="card-text fw-bold">Kategori Sampah</h5>
+        </div>
+        <div class="col-6 col-md-4 pl-5 text-end">
+            <a href="/kategori" class="text-decoration-none text-end">Lainnya</a>
+        </div>
+      </div>
+
+      {{-- card --}}
+      <div class="row mt-3">
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <a href="/jenis" class="text-decoration-none">
+                <div class="card">
+                    <img src="https://source.unsplash.com/300x70?fan" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Peralatan pertukaran suhu</h5>                 
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-6">
+            <a href="/jenis" class="text-decoration-none">
+                <div class="card">
+                    <img src="https://source.unsplash.com/300x70?monitor" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Monitor</h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+      </div>
+
+
+</div>
+
+@endsection
--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -1,3 +1,46 @@
 @extends('layouts.main')
 
 @section('container')
+
+<div class="container">
+    <div class="row mt-4">
+        <div class=" ">
+            <a href="/" class="text-black">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+                  </svg>
+            </a>
+            <span class="ml-2 fs-4 font-weight-bold t-1">Kategori Sampah</span>
+        </div>
+    </div>
+</div>
+
+<div class="row mt-4">
+    <a href="/jenis" class="text-decoration-none">
+        <div class="card mb-3">
+            <img src="https://source.unsplash.com/700x150?lamp" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Lampu</h5>
+            </div>
+          </div>
+    </a>
+    <a href="/jenis" class="text-decoration-none">
+        <div class="card mb-3">
+            <img src="https://source.unsplash.com/700x150?fan" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Peralatan pertukaran suhu</h5>
+            </div>
+          </div>
+    </a>
+    <a href="/jenis" class="text-decoration-none">
+        <div class="card mb-3">
+            <img src="https://source.unsplash.com/700x150?laptop" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Layar dan Monitor</h5>
+            </div>
+          </div>
+    </a>
+    
+</div>
+
+@endsection
