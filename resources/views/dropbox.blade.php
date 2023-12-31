e0e390a9bb683a0c9da86f4a5999a5e7c3c4cebc
litayusdiafatimah <litayusdiaf123@gmail.com>
2023-12-31 14:43:46 +0700

    Tampilan dropbox dan maps

--- resources/views/dropbox.blade.php
+++ resources/views/dropbox.blade.php
@@ -1,3 +1,23 @@
 @extends('layouts.main')
 
 @section('container')
+    <div class="card text-center mt-3">
+        <h1 class=" bg-white">
+            Dropbox terdekat
+        </h1>
+        <div class="card-body mt-2">
+            <img src="https://source.unsplash.com/735x335?maps" alt="" srcset="">
+        </div>
+        <div class="card-footer text-black d-flex justify-content-center align-items-center" style="background-color: #9BDCBD;">
+    <div class="ms-auto">
+        <h4><i class="fas fa-map-marker-alt me-2"></i>Jalan Cilame Indah no.40</h4>
+        <h5>0.5 km</h5>
+    </div>
+    <div class="ms-auto">
+        <a href="/maps" class="btn btn-light text-black fw-semibold">Jalan</a>
+    </div>
+</div>
+
+    </div>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+@endsection
--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -1,3 +1,27 @@
 @extends('layouts.main')
 
 @section('container')
+    <div class="card text-center mt-3">
+        <div style="position: relative;">
+            <img src="https://source.unsplash.com/1110x445?maps" alt="" srcset="">
+            <div class="card-body mt-2" style="position: absolute; top: 10%; left: 50%; transform: translate(-50%, -50%);">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><i class="fas fa-solid fa-circle fa-xs me-2"></i>Lokasi saya</li>
+                    <li class="list-group-item"><i class="fas fa-map-marker-alt me-2"></i>Jl Bukit Permata no.27</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="card-footer text-black d-flex justify-content-center align-items-center" style="background-color: #9BDCBD;" >
+            <div class="ms-auto">
+                <h2>30 Min</h2>
+                <h5>13.6 km <i class="fas fa-solid fa-circle fa-0x"></i> Rute tercepat</h5>
+            </div>
+            <div class="ms-auto">
+                <i class="fas fa-thin fa-car-side fa-4x"></i>
+            </div>
+        </div>
+
+    </div>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+@endsection
